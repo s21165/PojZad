0a977b2d02011e35b986ca4c3e791c0c49871d03
s21165 <S21165@pjwstk.edu.pl>
2020-05-11 03:29:55 +0200

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+
+
+
+
+class Main {
+
+    public static void main(String[] args){
+		private static String swiat="Witaj java";
+        System.out.println(swiat);
+    }
+}
+
